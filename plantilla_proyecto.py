ab48ae45db28a6d63670eb61621f434792182892
Luisa Gomez <89656628+4591526@users.noreply.github.com>
2026-06-19 20:01:08 -0500

    Add files via upload

---
+++ plantilla_proyecto.py
@@ -0,0 +1,26 @@
+# Importamos todas las librerías necesarias
+import streamlit as st
+import pandas as pd
+
+# Asignamos una variable a nuestra base de datos
+
+# Configuramos la página de Streamlit
+
+# Definimos las páginas de la barra lateral
+# https://docs.streamlit.io/develop/api-reference/layout/st.sidebar
+# st.sidebar.selectbox()
+
+# Definimos el contenido de cada página
+# https://docs.streamlit.io/develop/api-reference/text
+# st.markdown()
+# st.title()
+# st.header()
+# st.subheader()
+# st.write()
+
+# Inseramos imágenes en las páginas
+# https://docs.streamlit.io/develop/api-reference/media/st.image
+# st.image()
+# st.image(image, use_column_width=True)
+# st.image(image, caption='Texto de la imagen', width=300)
+
